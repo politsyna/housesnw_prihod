0b777d57537e1aa161a2c7f67539ca4295583be4
politsyna <v.politsyna@yandex.ru>
2017-03-19 23:02:03 +0300

    init

---
+++ src/Controller/Prihod.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\node_prihod\Controller;
+
+/**
+ * @file
+ * Contains \Drupal\node_orders\Controller\Page.
+ */
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Controller routines for page example routes.
+ */
+class Prihod extends ControllerBase {
+
+  /**
+   * Page Callback.
+   */
+  public static function kubatura($node) {
+    $diametr = $node->field_prihod_diametr->value;
+    $dlina = $node->field_prihod_dlina->value;
+    if (is_object($node)) {
+      $count = (3.1416 * pow($diametr / 100, 2)) / 4 * $dlina / 1000;
+    }
+    return $count;
+  }
+
+}
